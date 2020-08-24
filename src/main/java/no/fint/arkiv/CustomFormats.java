438d200e03ed811392834864d659c3fd9d8cf67a
Asgeir Nilsen <asgeir@fintlabs.no>
2020-08-24 11:41:24 +0200

    Added services for title and additional fields.

---
+++ src/main/java/no/fint/arkiv/CustomFormats.java
@@ -0,0 +1,17 @@
+package no.fint.arkiv;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Data
+@Component
+@EnableConfigurationProperties
+@ConfigurationProperties("fint.case.formats")
+public class CustomFormats {
+    private Map<String, String> title;
+    private Map<String, Map<String, String>> field;
+}
