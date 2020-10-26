c3af4faa86306381f7ac4436dd7258ca14896725
Asgeir Nilsen <asgeir@fintlabs.no>
2020-10-26 12:20:51 +0100

    Added Drosjeløyve

--- src/main/java/no/fint/arkiv/CaseDefaults.java
+++ src/main/java/no/fint/arkiv/CaseDefaults.java
@@ -13,5 +13,6 @@ public class CaseDefaults {
     private CaseProperties
             tilskuddfartoy,
             tilskuddfredabygningprivateie,
-            personalmappe;
+            personalmappe,
+            drosjeloyve;
 }
