b6b8bf2e9e308068dd0e5969feb1052728ade7f3
Ole Anders Eidjord <26850+oleanders@users.noreply.github.com>
2026-01-27 09:48:18 +0100

    Support for Java 8

--- src/main/java/no/novari/fint/arkiv/NoarkMetadataService.java
+++ src/main/java/no/novari/fint/arkiv/NoarkMetadataService.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
-import jakarta.annotation.PostConstruct;
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Map;
